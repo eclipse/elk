f132e24c2608012f39bee28a1171d03f5c1d9489
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2015-10-22 11:02:32 +0200

    Removed "core" from GMF and Graphiti plugins.

---
+++ plugins/org.eclipse.elk.graphiti/src/org/eclipse/elk/graphiti/GefDiagramLayoutManager.java
@@ -0,0 +1,146 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2015 Kiel University and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Kiel University - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.elk.graphiti;
+
+import org.eclipse.draw2d.Animation;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.elk.core.klayoutdata.KShapeLayout;
+import org.eclipse.elk.core.service.IDiagramLayoutManager;
+import org.eclipse.elk.core.service.LayoutMapping;
+import org.eclipse.elk.graph.KNode;
+import org.eclipse.elk.graph.properties.IProperty;
+import org.eclipse.elk.graph.properties.Property;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.GraphicalViewer;
+import org.eclipse.gef.editparts.ZoomManager;
+import org.eclipse.graphiti.ui.internal.parts.IPictogramElementEditPart;
+import org.eclipse.graphiti.ui.internal.util.gef.ScalableRootEditPartAnimated;
+
+/**
+ * An abstract diagram layout manager for GEF-based implementations.
+ * This variant is tuned for Graphiti diagram editors.
+ *
+ * @param <T> the type of diagram part that is handled by this diagram layout manager
+ * @author msp
+ * @kieler.design proposed by msp
+ * @kieler.rating proposed yellow by msp
+ */
+@SuppressWarnings("restriction")
+public abstract class GefDiagramLayoutManager<T> implements IDiagramLayoutManager<T> {
+
+    /** the animation time used for layout. */
+    public static final IProperty<Integer> ANIMATION_TIME = new Property<Integer>(
+            "gef.animationTime", 0);
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void applyLayout(final LayoutMapping<T> mapping, final boolean zoomToFit,
+            final int animationTime) {
+        mapping.setProperty(ANIMATION_TIME, animationTime);
+        Object layoutGraphObj = mapping.getParentElement();
+        if (zoomToFit && layoutGraphObj instanceof EditPart) {
+            // determine pre- or post-layout zoom
+            GraphicalViewer viewer = ((IPictogramElementEditPart) layoutGraphObj)
+                    .getConfigurationProvider().getDiagramContainer().getGraphicalViewer();
+            ZoomManager zoomManager = ((ScalableRootEditPartAnimated) viewer.getRootEditPart())
+                    .getZoomManager();
+            KNode parentNode = mapping.getLayoutGraph();
+            KShapeLayout parentLayout = parentNode.getData(KShapeLayout.class);
+            Dimension available = zoomManager.getViewport().getClientArea().getSize();
+            float desiredWidth = parentLayout.getWidth();
+            double scaleX = Math.min(available.width / desiredWidth, zoomManager.getMaxZoom());
+            float desiredHeight = parentLayout.getHeight();
+            double scaleY = Math.min(available.height / desiredHeight, zoomManager.getMaxZoom());
+            final double scale = Math.min(scaleX, scaleY);
+            final double oldScale = zoomManager.getZoom();
+
+            if (scale < oldScale) {
+                zoomManager.setViewLocation(new Point(0, 0));
+                zoomManager.setZoom(scale);
+                zoomManager.setViewLocation(new Point(0, 0));
+            }
+            applyLayout(mapping, animationTime);
+            if (scale > oldScale) {
+                zoomManager.setViewLocation(new Point(0, 0));
+                zoomManager.setZoom(scale);
+                zoomManager.setViewLocation(new Point(0, 0));
+            }
+        } else {
+            applyLayout(mapping, animationTime);
+        }
+    }
+    
+    /**
+     * Apply the computed layout to the original diagram.
+     * 
+     * @param mapping a layout mapping that was created by this layout manager
+     * @param animationTime the animation time in milliseconds, or 0 for no animation
+     */
+    private void applyLayout(final LayoutMapping<T> mapping, final int animationTime) {
+        // transfer layout to the diagram
+        transferLayout(mapping);
+        if (animationTime > 0) {
+            // apply the layout with animation
+            Animation.markBegin();
+            applyLayout(mapping);
+            Animation.run(animationTime);
+        } else {
+            // apply the layout without animation
+            applyLayout(mapping);
+        }
+    }
+
+    /**
+     * Transfer all layout data from the last created KGraph instance to the
+     * original diagram. The diagram is not modified yet, but all required
+     * preparations are performed.
+     * 
+     * @param mapping a layout mapping that was created by this layout manager
+     */
+    protected abstract void transferLayout(LayoutMapping<T> mapping);
+    
+    /**
+     * Apply the transferred layout to the original diagram. This final step
+     * is where the actual change to the diagram is done.
+     * 
+     * @param mapping a layout mapping that was created by this layout manager
+     */
+    protected abstract void applyLayout(LayoutMapping<T> mapping);
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void undoLayout(final LayoutMapping<T> mapping) {
+        int animationTime = mapping.getProperty(ANIMATION_TIME);
+        if (animationTime > 0) {
+            // undo the layout with animation
+            Animation.markBegin();
+            performUndo(mapping);
+            Animation.run(animationTime);
+        } else {
+            // undo the layout without animation
+            performUndo(mapping);
+        }
+    }
+    
+    /**
+     * Perform undo in the original diagram (optional operation).
+     * This implementation throws an {@code UnsupportedOperationException}.
+     *
+     * @param mapping a layout mapping that was created by this layout manager
+     */
+    protected void performUndo(final LayoutMapping<T> mapping) {
+        throw new UnsupportedOperationException("Undo is not supported by this layout manager.");
+    }
+
+}
---
+++ plugins/org.eclipse.elk.graphiti/src/org/eclipse/elk/graphiti/GraphitiLayoutCommand.java
@@ -0,0 +1,370 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2015 Kiel University and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Kiel University - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.elk.graphiti;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.elk.core.klayoutdata.KEdgeLayout;
+import org.eclipse.elk.core.klayoutdata.KInsets;
+import org.eclipse.elk.core.klayoutdata.KPoint;
+import org.eclipse.elk.core.klayoutdata.KShapeLayout;
+import org.eclipse.elk.core.klayoutdata.impl.KEdgeLayoutImpl;
+import org.eclipse.elk.core.klayoutdata.impl.KShapeLayoutImpl;
+import org.eclipse.elk.core.math.KVector;
+import org.eclipse.elk.core.math.KVectorChain;
+import org.eclipse.elk.core.math.ElkMath;
+import org.eclipse.elk.core.options.EdgeRouting;
+import org.eclipse.elk.core.options.LayoutOptions;
+import org.eclipse.elk.core.util.KimlUtil;
+import org.eclipse.elk.core.util.Pair;
+import org.eclipse.elk.core.util.nodespacing.Spacing.Margins;
+import org.eclipse.elk.graph.KEdge;
+import org.eclipse.elk.graph.KGraphElement;
+import org.eclipse.elk.graph.KLabel;
+import org.eclipse.elk.graph.KNode;
+import org.eclipse.elk.graph.KPort;
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.impl.LayoutContext;
+import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
+import org.eclipse.graphiti.mm.algorithms.styles.Point;
+import org.eclipse.graphiti.mm.pictograms.BoxRelativeAnchor;
+import org.eclipse.graphiti.mm.pictograms.ConnectionDecorator;
+import org.eclipse.graphiti.mm.pictograms.FixPointAnchor;
+import org.eclipse.graphiti.mm.pictograms.FreeFormConnection;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.graphiti.services.Graphiti;
+
+/**
+ * A command for applying the result of automatic layout to a Graphiti diagram.
+ * 
+ * @author msp
+ * @kieler.design proposed by msp
+ * @kieler.rating proposed yellow by msp
+ */
+public class GraphitiLayoutCommand extends RecordingCommand {
+    
+    /** list of graph elements and pictogram elements to layout. */
+    private List<Pair<KGraphElement, PictogramElement>> elements =
+            new LinkedList<Pair<KGraphElement, PictogramElement>>();
+    /** the feature provider for layout support. */
+    private IFeatureProvider featureProvider;
+    /** map of edge layouts to corresponding vector chains. */
+    private Map<KEdgeLayout, KVectorChain> bendpointsMap =
+            new HashMap<KEdgeLayout, KVectorChain>();
+
+    /**
+     * Creates a Graphiti layout command.
+     * 
+     * @param domain the transactional editing domain
+     * @param thefeatureProvider the feature provider
+     */
+    public GraphitiLayoutCommand(
+            final TransactionalEditingDomain domain,
+            final IFeatureProvider thefeatureProvider) {
+        super(domain, "Automatic Layout");
+        this.featureProvider = thefeatureProvider;
+    }
+    
+    /**
+     * Returns the feature provider.
+     * 
+     * @return the feature provider
+     */
+    protected IFeatureProvider getFeatureProvider() {
+        return featureProvider;
+    }
+
+    /**
+     * Adds the given element to this command, if it has been modified by a layout algorithm.
+     * 
+     * @param graphElement an element of the layout graph
+     * @param pictogramElement the corresponding pictogram element
+     */
+    public void add(final KGraphElement graphElement,
+            final PictogramElement pictogramElement) {
+        boolean modified = false;
+        if (graphElement instanceof KEdge) {
+            KEdgeLayoutImpl edgeLayout = graphElement.getData(KEdgeLayoutImpl.class);
+            modified = edgeLayout.isModified();
+        } else {
+            KShapeLayoutImpl shapeLayout = graphElement.getData(KShapeLayoutImpl.class);
+            modified = shapeLayout.isModified();
+        }
+        
+        if (modified) {
+            elements.add(new Pair<KGraphElement, PictogramElement>(graphElement,
+                    pictogramElement));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void doExecute() {
+        for (Pair<KGraphElement, PictogramElement> entry : elements) {
+            KGraphElement element = entry.getFirst();
+            if (element instanceof KPort) {
+                applyPortLayout((KPort) element, entry.getSecond());
+            } else if (element instanceof KNode) {
+                applyNodeLayout((KNode) element, entry.getSecond());
+            } else if (element instanceof KEdge) {
+                applyEdgeLayout((KEdge) element, entry.getSecond());
+            } else if (element instanceof KLabel
+                    && ((KLabel) element).eContainer() instanceof KEdge) {
+                applyEdgeLabelLayout((KLabel) element, entry.getSecond());
+            }
+        }
+        bendpointsMap.clear();
+    }
+
+    /**
+     * Apply layout for a port.
+     * 
+     * @param kport a port
+     * @param pelem the corresponding pictogram element
+     */
+    protected void applyPortLayout(final KPort kport, final PictogramElement pelem) {
+        KShapeLayout shapeLayout = kport.getData(KShapeLayout.class);
+        applyPortLayout(shapeLayout.getXpos(), shapeLayout.getYpos(), pelem, kport.getNode());
+    }
+    
+    /**
+     * Apply layout for a port.
+     * 
+     * @param xpos x position of the port
+     * @param ypos y position of the port
+     * @param pelem the pictogram element
+     * @param knode the node to which the port is connected
+     */
+    protected void applyPortLayout(final double xpos, final double ypos,
+            final PictogramElement pelem, final KNode knode) {
+        int offsetx = 0, offsety = 0;
+        if (pelem.getGraphicsAlgorithm() != null) {
+            offsetx = pelem.getGraphicsAlgorithm().getX();
+            offsety = pelem.getGraphicsAlgorithm().getY();
+        }
+        if (pelem instanceof BoxRelativeAnchor) {
+            BoxRelativeAnchor anchor = (BoxRelativeAnchor) pelem;
+            
+            KShapeLayout nodeLayout = knode.getData(KShapeLayout.class);
+            double relWidth = (xpos - offsetx) / nodeLayout.getWidth();
+            if (relWidth < 0) {
+                relWidth = 0;
+            }
+            if (relWidth > 1) {
+                relWidth = 1;
+            }
+            double relHeight = (ypos - offsety) / nodeLayout.getHeight();
+            if (relHeight < 0) {
+                relHeight = 0;
+            }
+            if (relHeight > 1) {
+                relHeight = 1;
+            }
+
+            anchor.setRelativeWidth(relWidth);
+            anchor.setRelativeHeight(relHeight);
+
+            featureProvider.layoutIfPossible(new LayoutContext(pelem));
+        } else if (pelem instanceof FixPointAnchor) {
+            FixPointAnchor anchor = (FixPointAnchor) pelem;
+
+            anchor.getLocation().setX((int) (xpos - offsetx));
+            anchor.getLocation().setY((int) (ypos - offsety));
+        }
+    }
+
+    /**
+     * Apply layout for a node.
+     * 
+     * @param knode a node
+     * @param pelem the corresponding pictogram element
+     */
+    protected void applyNodeLayout(final KNode knode, final PictogramElement pelem) {
+        KShapeLayout shapeLayout = knode.getData(KShapeLayout.class);
+        GraphicsAlgorithm ga = pelem.getGraphicsAlgorithm();
+        float xpos = shapeLayout.getXpos();
+        float ypos = shapeLayout.getYpos();
+        if (knode.getParent() != null) {
+            KShapeLayout parentLayout = knode.getParent().getData(KShapeLayout.class);
+            KInsets parentInsets = parentLayout.getInsets();
+            Margins parentMargins = parentLayout.getProperty(LayoutOptions.MARGINS);
+            xpos += parentMargins.left + parentInsets.getLeft();
+            ypos += parentMargins.top + parentInsets.getTop();
+        }
+        
+        float width = shapeLayout.getWidth();
+        float height = shapeLayout.getHeight();
+        Margins nodeMargins = shapeLayout.getProperty(LayoutOptions.MARGINS);
+        xpos -= nodeMargins.left;
+        ypos -= nodeMargins.top;
+        width += nodeMargins.left + nodeMargins.right;
+        height += nodeMargins.top + nodeMargins.bottom;
+        
+        ga.setX(Math.round(xpos));
+        ga.setY(Math.round(ypos));
+        ga.setWidth(Math.round(width));
+        ga.setHeight(Math.round(height));
+        featureProvider.layoutIfPossible(new LayoutContext(pelem));
+    }
+
+    /**
+     * Apply layout for an edge.
+     * 
+     * @param kedge an edge
+     * @param pelem the corresponding pictogram element
+     */
+    protected void applyEdgeLayout(final KEdge kedge, final PictogramElement pelem) {
+        // create bend points for the edge
+        KVectorChain bendPoints = getBendPoints(kedge);
+
+        if (pelem instanceof FreeFormConnection) {
+            FreeFormConnection connection = (FreeFormConnection) pelem;
+            List<Point> pointList = connection.getBendpoints();
+            // add the bend points to the connection, reusing existing points
+            for (int i = 0; i < bendPoints.size(); i++) {
+                KVector kpoint = bendPoints.get(i);
+                if (i >= pointList.size()) {
+                    Point point = Graphiti.getGaService().createPoint((int) Math.round(kpoint.x),
+                                    (int) Math.round(kpoint.y));
+                    pointList.add(point);
+                } else {
+                    Point point = pointList.get(i);
+                    point.setX((int) Math.round(kpoint.x));
+                    point.setY((int) Math.round(kpoint.y));
+                }
+            }
+            while (pointList.size() > bendPoints.size()) {
+                pointList.remove(pointList.size() - 1);
+            }
+            
+            // set source anchor position, if not already set via a port
+            if (kedge.getSourcePort() == null) {
+                KNode source = kedge.getSource();
+                KPoint sourcePoint = kedge.getData(KEdgeLayout.class).getSourcePoint();
+                float xpos = sourcePoint.getX(), ypos = sourcePoint.getY();
+                if (KimlUtil.isDescendant(kedge.getTarget(), source)) {
+                    KInsets insets = source.getData(KShapeLayout.class).getInsets();
+                    xpos += insets.getLeft();
+                    ypos += insets.getTop();
+                } else {
+                    KShapeLayout sourceLayout = source.getData(KShapeLayout.class);
+                    xpos -= sourceLayout.getXpos();
+                    ypos -= sourceLayout.getYpos();
+                }
+                applyPortLayout(xpos, ypos, connection.getStart(), source);
+            }
+            
+            // set target anchor position, if not already set via a port
+            if (kedge.getTargetPort() == null) {
+                KNode target = kedge.getTarget();
+                KVector targetPoint = kedge.getData(KEdgeLayout.class).getTargetPoint().createVector();
+                KNode referenceNode = kedge.getSource();
+                if (!KimlUtil.isDescendant(target, referenceNode)) {
+                    referenceNode = referenceNode.getParent();
+                }
+                KimlUtil.toAbsolute(targetPoint, referenceNode);
+                KimlUtil.toRelative(targetPoint, target);
+                applyPortLayout(targetPoint.x, targetPoint.y, connection.getEnd(), target);
+            }
+        }
+    }
+    
+    /**
+     * Get a vector chain holding the bend points for the given edge.
+     * 
+     * @param edge a layout edge
+     * @return the bend points for the edge
+     */
+    public KVectorChain getBendPoints(final KEdge edge) {
+        KEdgeLayout edgeLayout = edge.getData(KEdgeLayout.class);
+        KVectorChain bendPoints = bendpointsMap.get(edgeLayout);
+        if (bendPoints == null) {
+            // determine the offset for all bend points
+            KNode parent = edge.getSource();
+            if (!KimlUtil.isDescendant(edge.getTarget(), parent)) {
+                parent = parent.getParent();
+            }
+            KVector offset = new KVector();
+            KimlUtil.toAbsolute(offset, parent);
+
+            // gather the bend points of the edge
+            bendPoints = new KVectorChain();
+            KPoint sourcePoint = edgeLayout.getSourcePoint();
+            bendPoints.add(sourcePoint.getX() + offset.x, sourcePoint.getY() + offset.y);
+            for (KPoint bendPoint : edgeLayout.getBendPoints()) {
+                bendPoints.add(bendPoint.getX() + offset.x, bendPoint.getY() + offset.y);
+            }
+            KPoint targetPoint = edgeLayout.getTargetPoint();
+            bendPoints.add(targetPoint.getX() + offset.x, targetPoint.getY() + offset.y);
+
+            // transform spline control points into approximated bend points
+            EdgeRouting edgeRouting = edgeLayout.getProperty(LayoutOptions.EDGE_ROUTING);
+            if (edgeRouting == EdgeRouting.SPLINES
+                    && edgeLayout.getBendPoints().size() >= 1) {
+                bendPoints = ElkMath.approximateBezierSpline(bendPoints);
+            }
+            
+            bendPoints.removeFirst();
+            bendPoints.removeLast();
+            bendpointsMap.put(edgeLayout, bendPoints);
+        }
+        return bendPoints;
+    }
+
+    /**
+     * Apply layout for an edge label.
+     * 
+     * @param klabel an edge label
+     * @param pelem the corresponding pictogram element
+     */
+    protected void applyEdgeLabelLayout(final KLabel klabel,
+            final PictogramElement pelem) {
+        GraphicsAlgorithm ga = pelem.getGraphicsAlgorithm();
+        ConnectionDecorator decorator = (ConnectionDecorator) pelem;
+        KEdge kedge = (KEdge) klabel.eContainer();
+
+        // get vector chain for the bend points of the edge
+        KVectorChain bendPoints = new KVectorChain(getBendPoints(kedge));
+        KVector sourcePoint = KimlGraphitiUtil.calculateAnchorEnds(kedge.getSource(),
+                kedge.getSourcePort(), null);
+        bendPoints.addFirst(sourcePoint);
+        KVector targetPoint = KimlGraphitiUtil.calculateAnchorEnds(kedge.getTarget(),
+                kedge.getTargetPort(), null);
+        bendPoints.addLast(targetPoint);
+
+        // calculate reference point for the label
+        KVector referencePoint;
+        if (decorator.isLocationRelative()) {
+            referencePoint = bendPoints.pointOnLine(decorator.getLocation()
+                            * bendPoints.totalLength());
+        } else {
+            referencePoint = bendPoints.pointOnLine(decorator.getLocation());
+        }
+        
+        KShapeLayout shapeLayout = klabel.getData(KShapeLayout.class);
+        KVector position = shapeLayout.createVector();
+        KNode parent = kedge.getSource();
+        if (!KimlUtil.isDescendant(kedge.getTarget(), parent)) {
+            parent = parent.getParent();
+        }
+        KimlUtil.toAbsolute(position, parent);
+        ga.setX((int) Math.round(position.x - referencePoint.x));
+        ga.setY((int) Math.round(position.y - referencePoint.y));
+    }
+
+}
