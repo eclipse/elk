8a7d0801eaa52a647645973cec4f8308a3c8c0d8
Ulf Rueegg <uru@informatik.uni-kiel.de>
2016-02-17 00:28:13 +0100

    Added new layerer implementations
    (re-implementation of the network simplex layerer and two experimental layerers: minWidth and stretchWidth)

--- plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/p2layers/LayeringStrategy.java
+++ plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/p2layers/LayeringStrategy.java
@@ -15,11 +15,6 @@ import org.eclipse.elk.alg.layered.ILayoutPhaseFactory;
 
 /**
  * Enumeration of and factory for the different available layering strategies.
- * 
- * @author pdo
- * @author cds
- * @kieler.design 2012-08-10 chsch grh
- * @kieler.rating yellow 2012-11-13 review KI-33 by grh, akoc
  */
 public enum LayeringStrategy implements ILayoutPhaseFactory {
 
@@ -36,8 +31,18 @@ public enum LayeringStrategy implements ILayoutPhaseFactory {
      * as given in the input diagram are considered here. This means that if the user moves
      * a node, that movement is reflected in the layering of the graph.
      */
-    INTERACTIVE;
-
+    INTERACTIVE,
+    /**
+     * Nodes are put into Layers according to the average out-degree and their rank.
+     * Similar to {@link #LONGEST_PATH}, however, tries to reduce the maximum number of 
+     * nodes per layer.
+     */
+    EXP_STRETCH_WIDTH,
+    /**
+     * Implementation of the 'MinWidht' heuristic for solving the NP-hard minimum-width layering
+     * problem with consideration of dummy nodes.
+     */
+    EXP_MIN_WIDTH;
     
     /**
      * {@inheritDoc}
@@ -53,6 +58,12 @@ public enum LayeringStrategy implements ILayoutPhaseFactory {
         case INTERACTIVE:
             return new InteractiveLayerer();
             
+        case EXP_STRETCH_WIDTH:
+            return new StretchWidthLayerer();
+          
+        case EXP_MIN_WIDTH:
+            return new MinWidthLayerer();
+
         default:
             throw new IllegalArgumentException(
                     "No implementation is available for the layerer " + this.toString());
