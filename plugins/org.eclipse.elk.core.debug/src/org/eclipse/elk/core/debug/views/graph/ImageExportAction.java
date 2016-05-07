71bb8c2f542fa6c728ecaa6f27fbef029bb1f8e0
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2016-05-07 17:15:24 +0200

    Migrated old KIML Debug plug-in over to ELK, with some changes.
    Signed-off-by: Christoph Daniel Schulze <cds@informatik.uni-kiel.de>

---
+++ plugins/org.eclipse.elk.core.debug/src/org/eclipse/elk/core/debug/views/graph/ImageExportAction.java
@@ -0,0 +1,173 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2016 Kiel University and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Kiel University - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.elk.core.debug.views.graph;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.elk.core.debug.ElkDebugPlugin;
+import org.eclipse.elk.core.klayoutdata.KShapeLayout;
+import org.eclipse.elk.core.ui.rendering.GraphRenderingCanvas;
+import org.eclipse.elk.graph.KNode;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTException;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+
+/**
+ * Action that exports the currently displayed layout graph into an image file.
+ */
+public class ImageExportAction extends Action {
+
+    /** identifier string for this action. */
+    private static final String ACTION_ID = "org.eclipse.elk.debug.imageExport";
+    /** relative path to the icon to use for this action. */
+    private static final String ICON_PATH = "icons/export.gif";
+    /** preference identifier for the last used file name. */
+    private static final String LAST_FILE_NAME_PREF = "imageExportAction.lastImageFile";
+
+    /** the layout graph view associated with this action. */
+    private LayoutGraphView view;
+
+    /**
+     * Creates an image export action for a given layout graph view.
+     * 
+     * @param theview layout graph view that created this action
+     */
+    public ImageExportAction(final LayoutGraphView theview) {
+        this.view = theview;
+        setId(ACTION_ID);
+        setText("&Export PNG");
+        setToolTipText("Export the current graph as a PNG image file.");
+        setImageDescriptor(ElkDebugPlugin.imageDescriptorFromPlugin(ElkDebugPlugin.PLUGIN_ID, ICON_PATH));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void run() {
+        final GraphRenderingCanvas canvas = view.getCanvas();
+        if (canvas != null) {
+            // let the user select an output file
+            final String fileName = getFileName(canvas.getDisplay());
+
+            if (fileName != null) {
+                // create a job for painting and exporting the image
+                Job saveJob = new Job("Export PNG Image") {
+                    protected IStatus run(final IProgressMonitor monitor) {
+                        try {
+                            monitor.beginTask("Export PNG Image", 2);
+
+                            // paint the layout graph
+                            KNode graph = canvas.getLayoutGraph();
+                            KShapeLayout graphSize = graph.getData(KShapeLayout.class);
+                            Rectangle area = new Rectangle(0, 0, (int) graphSize.getWidth() + 1,
+                                    (int) graphSize.getHeight() + 1);
+                            Image image = new Image(canvas.getDisplay(), area.width, area.height);
+                            canvas.getRenderer().markDirty(area);
+                            canvas.getRenderer().render(graph, new GC(image), area);
+                            monitor.worked(1);
+                            if (monitor.isCanceled()) {
+                                return new Status(IStatus.INFO, ElkDebugPlugin.PLUGIN_ID, 0, "Aborted", null);
+                            }
+
+                            // save the image into the selected file
+                            ImageLoader imageLoader = new ImageLoader();
+                            ImageData[] imageData = new ImageData[] {image.getImageData()};
+                            imageLoader.data = imageData;
+                            imageLoader.save(fileName, SWT.IMAGE_PNG);
+                            monitor.worked(1);
+
+                            return new Status(IStatus.INFO, ElkDebugPlugin.PLUGIN_ID, 0, "OK", null);
+                        } catch (SWTException exception) {
+                            return new Status(IStatus.ERROR, ElkDebugPlugin.PLUGIN_ID,
+                                    exception.code, "Could not save the selected PNG file.", exception);
+                        } finally {
+                            monitor.done();
+                        }
+                    }
+                };
+
+                // process the image export job
+                IProgressMonitor monitor = Job.getJobManager().createProgressGroup();
+                saveJob.setProgressGroup(monitor, 2);
+                saveJob.setPriority(Job.SHORT);
+                saveJob.setUser(true);
+                saveJob.schedule();
+            }
+        }
+    }
+
+    /**
+     * Opens a file dialog for the user to choose an output file for the
+     * exported image.
+     * 
+     * @param display display to use as parent for the file dialog
+     * @return file name, or null if no file was selected
+     */
+    private static String getFileName(final Display display) {
+        IPreferenceStore preferenceStore = ElkDebugPlugin.getDefault().getPreferenceStore();
+
+        // create and configure file dialog
+        FileDialog fileDialog = new FileDialog(display.getActiveShell(), SWT.SAVE);
+        String[] extensions = new String[] {"*.png"};
+        fileDialog.setFilterExtensions(extensions);
+        String[] names = new String[] {"Portable Network Graphics files"};
+        fileDialog.setFilterNames(names);
+        fileDialog.setOverwrite(true);
+        fileDialog.setText("Select Output File");
+        fileDialog.setFileName(preferenceStore.getString(LAST_FILE_NAME_PREF));
+
+        // open the file dialog and check the output
+        String fileName = checkFileName(fileDialog.open());
+
+        if (fileName != null) {
+            preferenceStore.setValue(LAST_FILE_NAME_PREF, fileName);
+        }
+        return fileName;
+    }
+
+    /**
+     * Checks whether the extension of the given file name is compatible with
+     * the PNG format and changes it if needed.
+     * 
+     * @param fileName file name given by the user
+     * @return file name with .png as extension
+     */
+    private static String checkFileName(final String fileName) {
+        if (fileName == null) {
+            return null;
+        } else {
+            String nameCopy = new String(fileName).toLowerCase();
+            if (nameCopy.endsWith(".png")) {
+                return fileName;
+            } else if (nameCopy.endsWith(".pn")) {
+                return fileName + "g";
+            } else if (nameCopy.endsWith(".p")) {
+                return fileName + "ng";
+            } else if (nameCopy.endsWith(".")) {
+                return fileName + "png";
+            } else {
+                return fileName + ".png";
+            }
+        }
+    }
+
+}
---
+++ plugins/org.eclipse.elk.core.debug/src/org/eclipse/elk/core/debug/views/graph/LoadGraphAction.java
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2016 Kiel University and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Kiel University - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.elk.core.debug.views.graph;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.eclipse.elk.core.IGraphLayoutEngine;
+import org.eclipse.elk.core.RecursiveGraphLayoutEngine;
+import org.eclipse.elk.core.data.LayoutMetaDataService;
+import org.eclipse.elk.core.data.LayoutOptionData;
+import org.eclipse.elk.core.debug.ElkDebugPlugin;
+import org.eclipse.elk.core.klayoutdata.KShapeLayout;
+import org.eclipse.elk.core.options.CoreOptions;
+import org.eclipse.elk.core.util.BasicProgressMonitor;
+import org.eclipse.elk.core.util.IElkProgressMonitor;
+import org.eclipse.elk.graph.KGraphData;
+import org.eclipse.elk.graph.KNode;
+import org.eclipse.elk.graph.PersistentEntry;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.common.util.WrappedException;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+
+/**
+ * An action for loading a KGraph and performing layout on it.
+ */
+public class LoadGraphAction extends Action {
+    
+    /** identifier string for this action. */
+    private static final String ACTION_ID = "org.eclipse.elk.debug.loadGraph";
+    /** relative path to the icon to use for this action. */
+    private static final String ICON_PATH = "icons/import.gif";
+    /** preference identifier for the last used file name. */
+    private static final String LAST_FILE_NAME_PREF = "loadGraphAction.lastGraphFile";
+    
+    /**
+     * Creates a load graph action.
+     */
+    public LoadGraphAction() {
+        setId(ACTION_ID);
+        setText("Load Graph");
+        setToolTipText("Load, layout, and display a saved ELK graph.");
+        setImageDescriptor(ElkDebugPlugin.imageDescriptorFromPlugin(ElkDebugPlugin.PLUGIN_ID, ICON_PATH));
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void run() {
+        IPreferenceStore preferenceStore = ElkDebugPlugin.getDefault().getPreferenceStore();
+
+        // create and configure file dialog
+        FileDialog fileDialog = new FileDialog(Display.getDefault().getActiveShell(), SWT.OPEN);
+        fileDialog.setFilterExtensions(new String[] { "*.kgraph", "*.kgx", "*.kgt", "*.xmi", "*.*" });
+        fileDialog.setText("Select Graph File");
+        fileDialog.setFileName(preferenceStore.getString(LAST_FILE_NAME_PREF));
+
+        // open the file dialog and wait for file name
+        String fileName = fileDialog.open();
+        
+        if (fileName != null) {
+            preferenceStore.setValue(LAST_FILE_NAME_PREF, fileName);
+            
+            // load the file content
+            ResourceSet resourceSet = new ResourceSetImpl();
+            URI uri = URI.createFileURI(fileName);
+            Resource resource = resourceSet.createResource(uri);
+            try {
+                resource.load(null);
+                KNode content = (KNode) resource.getContents().get(0);
+                layout(content);
+            } catch (IOException exception) {
+                throw new WrappedException(exception);
+            }
+        }
+    }
+    
+    /**
+     * Perform layout and draw the resulting graph.
+     * 
+     * @param graph a graph
+     */
+    private void layout(final KNode graph) {
+        // deserialize layout options
+        LayoutMetaDataService dataService = LayoutMetaDataService.getInstance();
+        Iterator<EObject> contentIter = graph.eAllContents();
+        while (contentIter.hasNext()) {
+            EObject obj = contentIter.next();
+            if (obj instanceof KGraphData) {
+                KGraphData graphData = (KGraphData) obj;
+                for (PersistentEntry entry : graphData.getPersistentEntries()) {
+                    LayoutOptionData optionData = dataService.getOptionData(entry.getKey());
+                    
+                    if (optionData != null) {
+                        Object value = optionData.parseValue(entry.getValue());
+                        
+                        if (value != null) {
+                            graphData.setProperty(optionData, value);
+                        }
+                    }
+                }
+            }
+        }
+        
+        // perform layout using a graph layout engine
+        KShapeLayout graphLayout = graph.getData(KShapeLayout.class);
+        IElkProgressMonitor monitor = new BasicProgressMonitor();
+        if (graphLayout != null && !graphLayout.getProperty(CoreOptions.NO_LAYOUT)) {
+            IGraphLayoutEngine layoutEngine = new RecursiveGraphLayoutEngine();
+            layoutEngine.layout(graph, monitor);
+        }
+        
+        // draw the resulting layout on the canvas
+        LayoutGraphView.updateWithGraph(graph);
+    }
+
+}
