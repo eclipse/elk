a1c1d6dfa8b38f6e61dbef94e0fcd1216178732b
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2018-08-28 09:06:43 +0200

    Added missing license headers.

--- test/org.eclipse.elk.alg.test/src/org/eclipse/elk/alg/test/framework/util/PlainJavaInitialization.java
+++ test/org.eclipse.elk.alg.test/src/org/eclipse/elk/alg/test/framework/util/PlainJavaInitialization.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Kiel University and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipse.elk.alg.test.framework.util;
 
 import org.eclipse.elk.alg.disco.options.DisCoMetaDataProvider;
