f9cc54ead33c4b98174bfadb5a5219e5341363b1
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2019-08-02 11:06:35 +0200

    Tests: Fixed wrong test graph creation. #399
    The failing tests tested crossing minimization as it runs on
    hierarchical graphs, but did not configure the test graph such that
    hierarchy was considered by the algorithm. This, it didn't do anything
    and the tests failed.
    
    Signed-off-by: Christoph Daniel Schulze <cds@informatik.uni-kiel.de>

--- test/org.eclipse.elk.alg.layered.test/src/org/eclipse/alg/layered/intermediate/greedyswitch/TestGraphCreator.java
+++ test/org.eclipse.elk.alg.layered.test/src/org/eclipse/alg/layered/intermediate/greedyswitch/TestGraphCreator.java
@@ -27,6 +27,7 @@ import org.eclipse.elk.alg.layered.options.GraphProperties;
 import org.eclipse.elk.alg.layered.options.InternalProperties;
 import org.eclipse.elk.alg.layered.options.LayeredOptions;
 import org.eclipse.elk.core.options.EdgeRouting;
+import org.eclipse.elk.core.options.HierarchyHandling;
 import org.eclipse.elk.core.options.PortConstraints;
 import org.eclipse.elk.core.options.PortSide;
 import org.eclipse.elk.graph.properties.IProperty;
@@ -73,6 +74,7 @@ public class TestGraphCreator {
         setUpIds();
         g.setProperty(LayeredOptions.EDGE_ROUTING, EdgeRouting.ORTHOGONAL);
         g.setProperty(InternalProperties.RANDOM, random);
+        g.setProperty(LayeredOptions.HIERARCHY_HANDLING, HierarchyHandling.INCLUDE_CHILDREN);
         return g;
     }
 
