b5f4a82129591613d8813695b6cf3464c9f9e2f8
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2019-12-05 16:13:58 +0100

    General: Remove contributors from file headers.

--- plugins/org.eclipse.elk.alg.common/src/org/eclipse/elk/alg/common/networksimplex/NNode.java
+++ plugins/org.eclipse.elk.alg.common/src/org/eclipse/elk/alg/common/networksimplex/NNode.java
@@ -4,9 +4,6 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Kiel University - initial API and implementation
  *******************************************************************************/
 package org.eclipse.elk.alg.common.networksimplex;
 
