1d14de6f1a417599ad896a46f88bc69934cc39e7
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2019-12-05 22:48:20 +0100

    General: Added missing file headers.

--- plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/p3order/GreedyPortDistributor.java
+++ plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/p3order/GreedyPortDistributor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, Kiel University.
+ * Copyright (c) 2016 Kiel University.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
