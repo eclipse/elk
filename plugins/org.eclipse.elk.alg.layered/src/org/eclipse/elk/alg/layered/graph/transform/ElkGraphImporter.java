4d46ef5410a3ff0070982ec884fd3648566168ad
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2020-02-28 15:17:13 +0100

    Layered: Fixed hyperedges with merge edges set to true. #504
    Signed-off-by: Christoph Daniel Schulze <cds@informatik.uni-kiel.de>

--- plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/graph/transform/ElkGraphImporter.java
+++ plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/graph/transform/ElkGraphImporter.java
@@ -873,11 +873,6 @@ class ElkGraphImporter {
                 lport.getLabels().add(transformLabel(elklabel));
             }
         }
-
-        // Check if we need to add anything to the graph properties
-        if (elkport.getIncomingEdges().size() + elkport.getOutgoingEdges().size() > 1) {
-            graphProperties.add(GraphProperties.HYPEREDGES);
-        }
         
         switch (layoutDirection) {
         case LEFT:
@@ -1015,6 +1010,13 @@ class ElkGraphImporter {
         // Finally set the source and target of the edge
         ledge.setSource(sourceLPort);
         ledge.setTarget(targetLPort);
+        
+        // If the ports have multiple incoming or outgoing edges, the HYPEREDGE property needs to be set
+        if (sourceLPort.getIncomingEdges().size() > 1 || sourceLPort.getOutgoingEdges().size() > 1
+                || targetLPort.getIncomingEdges().size() > 1 || targetLPort.getOutgoingEdges().size() > 1) {
+            
+            graphProperties.add(GraphProperties.HYPEREDGES);
+        }
 
         // Transform the edge's labels
         for (ElkLabel elklabel : elkedge.getLabels()) {
