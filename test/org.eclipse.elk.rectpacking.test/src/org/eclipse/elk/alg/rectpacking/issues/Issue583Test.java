28ca334d99f386f0356b4384a55ce4144ffb968a
Soeren Domroes <sdo@informatik.uni-kiel.de>
2020-05-04 08:17:31 +0200

    Fixed broken header and wrong automatic-modulname.
    Signed-off-by: Soeren Domroes <sdo@informatik.uni-kiel.de>

--- test/org.eclipse.elk.rectpacking.test/src/org/eclipse/elk/alg/rectpacking/issues/Issue583Test.java
+++ test/org.eclipse.elk.rectpacking.test/src/org/eclipse/elk/alg/rectpacking/issues/Issue583Test.java
@@ -4,8 +4,9 @@
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
- * SPDX-License-Identifier: EPL-2.0  *******************************************************************************/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 package org.eclipse.elk.alg.rectpacking.issues;
 
 import static org.junit.Assert.assertEquals;
