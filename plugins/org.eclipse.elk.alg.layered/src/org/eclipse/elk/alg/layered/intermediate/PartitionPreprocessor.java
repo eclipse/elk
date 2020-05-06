2afeb18ea26c31873ed9074aa475b9d59f26bc1b
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2020-05-06 14:41:22 +0200

    Layered: Fix PartitionPreprocessor to accept non-consecutive IDs. #592
    Signed-off-by: Christoph Daniel Schulze <cds@informatik.uni-kiel.de>

--- plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/intermediate/PartitionPreprocessor.java
+++ plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/intermediate/PartitionPreprocessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Kiel University and others.
+ * Copyright (c) 2016, 2020 Kiel University and others.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -9,8 +9,10 @@
  *******************************************************************************/
 package org.eclipse.elk.alg.layered.intermediate;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.elk.alg.layered.graph.LEdge;
 import org.eclipse.elk.alg.layered.graph.LGraph;
@@ -22,7 +24,8 @@ import org.eclipse.elk.core.alg.ILayoutProcessor;
 import org.eclipse.elk.core.options.PortSide;
 import org.eclipse.elk.core.util.IElkProgressMonitor;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 
 /**
  * Add constraint edges between partitions. </br>
@@ -43,71 +46,70 @@ import com.google.common.collect.Lists;
  *   <dt>Same-slot dependencies:</dt>
  *     <dd>None.</dd>
  * </dl>
- *
- * @see PartitionPostprocessor
  */
 public class PartitionPreprocessor implements ILayoutProcessor<LGraph> {
 
     /** The priority to set on added constraint edges. */
     private static final int PARTITION_CONSTRAINT_EDGE_PRIORITY = 20;
 
-    /** The nodes of the currently processed graph, sorted into partitions. */
-    private List<List<LNode>> partitions;
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
     public void process(final LGraph lGraph, final IElkProgressMonitor monitor) {
         monitor.begin("Adding partition constraint edges", 1);
-        partitions = Lists.newArrayList();
-        // Sort nodes into partitions.
-        for (LNode node : lGraph.getLayerlessNodes()) {
-            if (node.hasProperty(LayeredOptions.PARTITIONING_PARTITION)) {
-                Integer index = node.getProperty(LayeredOptions.PARTITIONING_PARTITION);
-                retrievePartition(index).add(node);
-            }
+        
+        // Collect nodes which have a partition set
+        Multimap<Integer, LNode> partitionToNodesMap = HashMultimap.create();
+        
+        // Go through all layerless nodes and collect all partition IDs in use
+        lGraph.getLayerlessNodes().stream()
+            .filter(node -> node.hasProperty(LayeredOptions.PARTITIONING_PARTITION))
+            .forEach(node -> partitionToNodesMap.put(node.getProperty(LayeredOptions.PARTITIONING_PARTITION), node));
+        
+        if (partitionToNodesMap.isEmpty()) {
+            // This shouldn't happen, but if it does, there's nothing to do
+            return;
         }
-
-        // Add edges from each node to every node in the next greater partition.
-        for (int i = 0; i < partitions.size() - 1; i++) {
-            for (LNode node : partitions.get(i)) {
-                LPort sourcePort = new LPort();
-                sourcePort.setNode(node);
-                sourcePort.setSide(PortSide.EAST);
-                sourcePort.setProperty(InternalProperties.PARTITION_DUMMY, true);
-                
-                for (LNode otherNode : partitions.get(i + 1)) {
-                    LPort targetPort = new LPort();
-                    targetPort.setNode(otherNode);
-                    targetPort.setSide(PortSide.WEST);
-                    targetPort.setProperty(InternalProperties.PARTITION_DUMMY, true);
-                    
-                    LEdge edge = new LEdge();
-                    edge.setProperty(InternalProperties.PARTITION_DUMMY, true);
-                    edge.setProperty(LayeredOptions.PRIORITY_DIRECTION, PARTITION_CONSTRAINT_EDGE_PRIORITY);
-                    edge.setSource(sourcePort);
-                    edge.setTarget(targetPort);
-                }
-            }
+        
+        // Collect a sorted list of partition IDs
+        List<Integer> sortedPartitionIDs = partitionToNodesMap.keySet().stream()
+            .sorted()
+            .collect(Collectors.toList());
+        
+        // For each pair of consecutive partition IDs (a, b), connect all nodes from partition a to partition b
+        Iterator<Integer> idIterator = sortedPartitionIDs.iterator();
+        
+        Integer firstId = idIterator.next();
+        while (idIterator.hasNext()) {
+            Integer secondId = idIterator.next();
+            connectNodes(partitionToNodesMap.get(firstId), partitionToNodesMap.get(secondId));
+            firstId = secondId;
         }
-        partitions = null;
+        
         monitor.done();
     }
-    
+
     /**
-     * Retrieve the partition with the given index. If doesn't exist yet, the list of partitions is
-     * extended sufficiently.
-     * 
-     * @param index
-     *            the index of the partition to retrieve.
-     * @return the existing or newly created partition.
+     * Connects all nodes from the first collection to all nodes from the second collection.
      */
-    private List<LNode> retrievePartition(final int index) {
-        while (index >= partitions.size()) {
-            partitions.add(new ArrayList<LNode>());
+    private void connectNodes(final Collection<LNode> firstPartition, final Collection<LNode> secondPartition) {
+        for (LNode node : firstPartition) {
+            LPort sourcePort = new LPort();
+            sourcePort.setNode(node);
+            sourcePort.setSide(PortSide.EAST);
+            sourcePort.setProperty(InternalProperties.PARTITION_DUMMY, true);
+            
+            for (LNode otherNode : secondPartition) {
+                LPort targetPort = new LPort();
+                targetPort.setNode(otherNode);
+                targetPort.setSide(PortSide.WEST);
+                targetPort.setProperty(InternalProperties.PARTITION_DUMMY, true);
+                
+                LEdge edge = new LEdge();
+                edge.setProperty(InternalProperties.PARTITION_DUMMY, true);
+                edge.setProperty(LayeredOptions.PRIORITY_DIRECTION, PARTITION_CONSTRAINT_EDGE_PRIORITY);
+                edge.setSource(sourcePort);
+                edge.setTarget(targetPort);
+            }
         }
-        return partitions.get(index);
     }
 
 }
