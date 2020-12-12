1432e1898ad8b9c166bd393c757318798ad8fbe5
le-cds <zephanya@web.de>
2020-12-12 12:37:54 +0100

    Core: Improve documentation of hierarchy handling.
    Signed-off-by: le-cds <zephanya@web.de>

--- plugins/org.eclipse.elk.core/src/org/eclipse/elk/core/options/HierarchyHandling.java
+++ plugins/org.eclipse.elk.core/src/org/eclipse/elk/core/options/HierarchyHandling.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Kiel University and others.
+ * Copyright (c) 2016, 2020 Kiel University and others.
  * 
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -10,45 +10,42 @@
 package org.eclipse.elk.core.options;
 
 /**
- * Options for setting how children of nodes should be handled in the current layout run. There are
- * three options for this:
- * <ul>
- * <li>INHERIT: The current node should implement the same behavior as the parent node. If the root
- * node is evaluated and it is set to inherit (or not set at all) the property is set to
- * {@link #SEPARATE_CHILDREN}.</li>
- * <li>INCLUDE_CHILDREN: The children of the current node should be included in the current layout
- * run. This enables edges to be routed across the boundary of hierarchy layers.</li>
- * <li>SEPARATE_CHILDREN: The children of the current node are layouted independently from their
- * parent node. The resulting layout information is then used to layout the parent node.</li>
- * </ul>
+ * Options for setting how children of nodes should be handled in the current layout run.
+ * 
  * <p>
- * The inclusion of further children can be disabled by setting the option back to SEPARATE_CHILDREN
- * at lower levels. For this option to have any effect, the option needs to be set to at least two
- * successive levels of hierarchy.
+ * The basic idea is this: If you want nodes to be laid out together across hierarchy levels, set hierarchy handling to
+ * {@link #INCLUDE_CHILDREN} on all nodes that should be laid out in one go. As soon as a node's content should be laid
+ * out in a separate layout run, set the node's hierarchy handling to {@link #SEPARATE_CHILDREN}.
+ * </p>
+ * 
+ * <p>
+ * If the layout algorithm doesn't support hierarchical layout, this property is ignored and the layout is calculated
+ * separately for each child hierarchy.
  * </p>
  * <p>
- * If the layout algorithm doesn't support hierarchical layout, this property is ignored and the
- * layout is calculated separately for each child hierarchy.
- * </p>
- * <p>
- * <i>Note:</i> Layout algorithms only need to differentiate between INCLUDE_CHILDREN and
- * SEPARATE_CHILDREN as the inheritance is evaluated by ELK.
+ * <i>Note:</i> Layout algorithms only need to differentiate between {@link #INCLUDE_CHILDREN} and
+ * {@link #SEPARATE_CHILDREN} as {@link #INHERIT} is evaluated and set to the appropriate more specific value by ELK.
  * </p>
  */
 public enum HierarchyHandling {
+
     /**
-     * Inherit the behavior from the parent node. If set on the root node, SEPARATE_CHILDREN is
-     * assumed.
+     * Inherit the parent node's hierarchy handling. The root node has no parent node; here, this setting defaults to
+     * {@link #SEPARATE_CHILDREN}.
      */
     INHERIT,
 
     /**
-     * Include the children in the parent layout run.
+     * Allows the node's children should be included in the current layout run. Which children are included in the
+     * layout run is determined by their hierarchy handling setting. For a child to actually be included, its hierarchy
+     * handling must be set to either {@link #INHERIT} or {@link #INCLUDE_CHILDREN}.
      */
     INCLUDE_CHILDREN,
 
     /**
-     * Perform a separate layout run for child nodes and layout the parent afterwards.
+     * Lays out the node with a new layout run. Even if its parent node is set to {@link #INCLUDE_CHILDREN}, this node
+     * will trigger a separate layout run and will thus not be included in the parent node's layout run.
      */
     SEPARATE_CHILDREN
+
 }
