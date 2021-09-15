26f62ddac9151019b324aa9285f65ffa492f8301
Sören Domrös <sdo@informatik.uni-kiel.de>
2021-09-15 12:18:56 +0200

    layered: Assign model order on import of hierarchical graphs. fixes #724 (#756)
    * layered: Assign model order on import of hierarchical graphs. fixes #724
    
    Signed-off-by: Soeren Domroes <sdo@informatik.uni-kiel.de>
    
    * layered: Only set model order property if required.
    
    Signed-off-by: Soeren Domroes <sdo@informatik.uni-kiel.de>
    
    * layered: Set model order really only if a strategy is set.
    
    Signed-off-by: Soeren Domroes <sdo@informatik.uni-kiel.de>
    
    * layered: Adapted to renaming of considerModelOrder to
    considerModelOrder.strategy.
    
    Signed-off-by: Soeren Domroes <sdo@informatik.uni-kiel.de>
    
    * layered: Adapted graph importer to model order cycle breaker changes.
    
    Signed-off-by: Soeren Domroes <sdo@informatik.uni-kiel.de>

--- plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/graph/transform/ElkGraphImporter.java
+++ plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/graph/transform/ElkGraphImporter.java
@@ -295,10 +295,19 @@ class ElkGraphImporter {
         
         Direction parentGraphDirection = lgraph.getProperty(LayeredOptions.DIRECTION);
 
+        // Model order index for nodes
+        int index = 0;
         // Transform the node's children
         elkGraphQueue.addAll(elkgraph.getChildren());
         while (!elkGraphQueue.isEmpty()) {
             ElkNode elknode = elkGraphQueue.poll();
+            
+            if (elkgraph.getProperty(LayeredOptions.CONSIDER_MODEL_ORDER_STRATEGY) != OrderingStrategy.NONE
+                    || elkgraph.getProperty(LayeredOptions.CYCLE_BREAKING_STRATEGY)
+                    == CycleBreakingStrategy.MODEL_ORDER) {
+                // Assign a model order to the nodes as they are read
+                elknode.setProperty(InternalProperties.MODEL_ORDER, index++);
+            }
             
             // Check if the current node is to be laid out in the first place
             boolean isNodeToBeLaidOut = !elknode.getProperty(LayeredOptions.NO_LAYOUT);
@@ -351,6 +360,8 @@ class ElkGraphImporter {
             }
         }
 
+        // Model order index for edges. 
+        index = 0;
         // Transform the edges
         elkGraphQueue.add(elkgraph);
         while (!elkGraphQueue.isEmpty()) {
@@ -359,6 +370,13 @@ class ElkGraphImporter {
             for (ElkEdge elkedge : elkGraphNode.getContainedEdges()) {
                 // We don't support hyperedges
                 checkEdgeValidity(elkedge);
+                
+                if (elkgraph.getProperty(LayeredOptions.CONSIDER_MODEL_ORDER_STRATEGY) != OrderingStrategy.NONE
+                        || elkgraph.getProperty(LayeredOptions.CYCLE_BREAKING_STRATEGY)
+                        == CycleBreakingStrategy.MODEL_ORDER) {
+                    // Assign a model order to the edges as they are read
+                    elkedge.setProperty(InternalProperties.MODEL_ORDER, index++);
+                }
                 
                 ElkNode sourceNode = ElkGraphUtil.connectableShapeToNode(elkedge.getSources().get(0));
                 ElkNode targetNode = ElkGraphUtil.connectableShapeToNode(elkedge.getTargets().get(0));
