a2e6c604e4347601d2f543e10d89161c637ee10b
Soeren Domroes <sdo@informatik.uni-kiel.de>
2022-03-16 15:56:06 +0100

    Recommit test and remove previous fix.
    Signed-off-by: Soeren Domroes <sdo@informatik.uni-kiel.de>

--- plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/p3order/BarycenterHeuristic.java
+++ plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/p3order/BarycenterHeuristic.java
@@ -91,12 +91,8 @@ public class BarycenterHeuristic implements ICrossingMinimizationHeuristic {
                 Collections.sort(layer, barycenterStateComparator);
             }
 
-            // Resolve ordering constraints, since the model order barycenter heuristic does not only use barycenters
-            // the constraints cannot be solved in that case.
-            // FIXME That looks like a bad thing to do.
-//            if (!layer.get(0).getGraph().getProperty(LayeredOptions.CROSSING_MINIMIZATION_FORCE_NODE_MODEL_ORDER)) {
-                constraintResolver.processConstraints(layer);
-//            }
+            // Resolve ordering constraints
+            constraintResolver.processConstraints(layer);
         }
     }
 
