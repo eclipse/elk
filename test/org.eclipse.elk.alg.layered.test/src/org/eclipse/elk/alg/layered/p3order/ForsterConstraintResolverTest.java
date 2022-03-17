40596a9cc778a114c1b87accdd9c2123694e7c64
Soeren Domroes <sdo@informatik.uni-kiel.de>
2022-03-17 08:05:28 +0100

    Added another test output.
    Signed-off-by: Soeren Domroes <sdo@informatik.uni-kiel.de>

--- test/org.eclipse.elk.alg.layered.test/src/org/eclipse/elk/alg/layered/p3order/ForsterConstraintResolverTest.java
+++ test/org.eclipse.elk.alg.layered.test/src/org/eclipse/elk/alg/layered/p3order/ForsterConstraintResolverTest.java
@@ -92,7 +92,8 @@ public class ForsterConstraintResolverTest {
                 if (newLayoutUnit != null && newLayoutUnit != currentLayoutUnit) {
                     // We have a node which does not belong to the current layout unit. That's okay if the new layout
                     // unit has not been encountered before (if it has, nodes of different layout units overlap)
-                    assertFalse(encounteredLayoutUnits.contains(newLayoutUnit));
+                    assertFalse("Node " + newLayoutUnit + " is overlapping with the current layout unit",
+                            encounteredLayoutUnits.contains(newLayoutUnit));
                     
                     if (currentLayoutUnit != null) {
                         encounteredLayoutUnits.add(currentLayoutUnit);
