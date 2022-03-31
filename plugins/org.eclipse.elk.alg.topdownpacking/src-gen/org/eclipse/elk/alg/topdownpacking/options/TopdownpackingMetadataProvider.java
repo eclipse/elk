f752627b78e013b280d8f5111723c3e1c94c409f
Max Kasperowski <mka@informatik.uni-kiel.de>
2022-03-31 09:10:21 +0200

    update structure of algorithm metadata

---
+++ plugins/org.eclipse.elk.alg.topdownpacking/src-gen/org/eclipse/elk/alg/topdownpacking/options/TopdownpackingMetadataProvider.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2022 Kiel University and others.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.elk.alg.topdownpacking.options;
+
+import org.eclipse.elk.core.data.ILayoutMetaDataProvider;
+
+@SuppressWarnings("all")
+public class TopdownpackingMetadataProvider implements ILayoutMetaDataProvider {
+  public void apply(final org.eclipse.elk.core.data.ILayoutMetaDataProvider.Registry registry) {
+    new org.eclipse.elk.alg.topdownpacking.options.TopdownpackingOptions().apply(registry);
+  }
+}
