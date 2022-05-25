c870a23d788c4e5b06c0981719912ef0b0dce23d
Max Kasperowski <mka@informatik.uni-kiel.de>
2022-05-25 15:35:04 +0200

    resolved todo

--- plugins/org.eclipse.elk.alg.topdownpacking/src/org/eclipse/elk/alg/topdownpacking/TopdownpackingLayoutProvider.java
+++ plugins/org.eclipse.elk.alg.topdownpacking/src/org/eclipse/elk/alg/topdownpacking/TopdownpackingLayoutProvider.java
@@ -18,13 +18,10 @@ import org.eclipse.elk.core.util.IElkProgressMonitor;
 import org.eclipse.elk.graph.ElkNode;
 
 /**
- * TODO: improve this documentation
  * A simple box packing algorithm that places nodes as evenly sized rectangles. This algorithm uses fixed sizes and 
- * therefore requires the option 'Topdown Layout' to be set to true to result in a correct layout.
+ * therefore requires the option 'Topdown Layout' to be set to true to result in a correct layout. Using this algorithm
+ * makes parallel nodes possible by allowing a node know its child layout size before the child layout is computed.
  * 
- * New more specific usage idea: for sccharts in topdown layout instead of rectpacking. parent state should already
- * be sized to fit this layout. Size can be predicted. This layout should NOT be scaled down, instead only its child layout,
- * which in the case of SCCharts is a layered layout.
  */
 public class TopdownpackingLayoutProvider extends AbstractLayoutProvider {
 
