cdb8f68f544da049f8c05ae35f2479aa5b0ffc7e
Max Kasperowski <mka@informatik.uni-kiel.de>
2022-05-31 09:27:11 +0200

    Add testing structure for topdown packing algorithm.

---
+++ test/org.eclipse.elk.alg.topdown.test/src/org/eclipse/elk/alg/topdown/test/TopdownPackingTest.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Kiel University and others.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * SPDX-License-Identifier: EPL-2.0 
+ *******************************************************************************/
+package org.eclipse.elk.alg.topdown.test;
+
+import static org.junit.Assert.fail;
+
+import org.eclipse.elk.alg.test.PlainJavaInitialization;
+import org.eclipse.elk.alg.topdownpacking.TopdownpackingLayoutProvider;
+import org.eclipse.elk.core.util.BasicProgressMonitor;
+import org.eclipse.elk.graph.ElkNode;
+import org.eclipse.elk.graph.util.ElkGraphUtil;
+import org.junit.Test;
+
+/**
+ * Tests the topdown packing algorithm. The node sizes must be externally set and determine the layout results.
+ */
+public class TopdownPackingTest {
+
+    // CHECKSTYLEOFF MagicNumber
+
+    /**
+     * Test topdown packing on an empty graph.
+     */
+    @Test
+    public void testEmptyGraph() {
+        PlainJavaInitialization.initializePlainJavaLayout();
+        ElkNode graph = createEmptyGraph();
+        TopdownpackingLayoutProvider layoutProvider = new TopdownpackingLayoutProvider();
+        layoutProvider.layout(graph, new BasicProgressMonitor());
+    }
+    
+    /**
+     * Test topdown packing for a graph with two nodes. In this case the resulting graph
+     * size should be vertically compacted.
+     */
+    @Test
+    public void testTwoNodes() {
+        fail();
+    }
+    
+    /**
+     * Test topdown packing for a graph with three nodes. In this case the third node should
+     * be expanded to fill the whitespace of the parent.
+     */
+    @Test
+    public void testThreeNodes() {
+        fail();
+    }
+    
+    /**
+     * Test topdown packing for a graph with five nodes. In this case there is both vertical
+     * compaction and whitespace elimination in the second row.
+     */
+    @Test
+    public void testFiveNodes() {
+        fail();
+    }
+    
+    private static ElkNode createEmptyGraph() {
+        return ElkGraphUtil.createGraph();
+    }
+}
