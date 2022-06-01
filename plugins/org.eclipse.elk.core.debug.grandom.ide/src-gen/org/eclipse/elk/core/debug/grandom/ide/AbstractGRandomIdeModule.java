3d2ce424e6ff2846b1eb0ff4090dee2d337c7d8e
Max Kasperowski <mka@informatik.uni-kiel.de>
2022-06-01 14:28:00 +0200

    revert changes

--- plugins/org.eclipse.elk.core.debug.grandom.ide/src-gen/org/eclipse/elk/core/debug/grandom/ide/AbstractGRandomIdeModule.java
+++ plugins/org.eclipse.elk.core.debug.grandom.ide/src-gen/org/eclipse/elk/core/debug/grandom/ide/AbstractGRandomIdeModule.java
@@ -22,8 +22,8 @@ import org.eclipse.xtext.ide.editor.contentassist.antlr.AntlrProposalConflictHel
 import org.eclipse.xtext.ide.editor.contentassist.antlr.IContentAssistParser;
 import org.eclipse.xtext.ide.editor.contentassist.antlr.internal.Lexer;
 import org.eclipse.xtext.ide.refactoring.IRenameStrategy2;
-import org.eclipse.xtext.ide.server.rename.IRenameService2;
-import org.eclipse.xtext.ide.server.rename.RenameService2;
+import org.eclipse.xtext.ide.server.rename.IRenameService;
+import org.eclipse.xtext.ide.server.rename.RenameService;
 
 /**
  * Manual modifications go to {@link GRandomIdeModule}.
@@ -54,8 +54,8 @@ public abstract class AbstractGRandomIdeModule extends DefaultIdeModule {
 	}
 	
 	// contributed by org.eclipse.xtext.xtext.generator.ui.refactoring.RefactorElementNameFragment2
-	public Class<? extends IRenameService2> bindIRenameService() {
-		return RenameService2.class;
+	public Class<? extends IRenameService> bindIRenameService() {
+		return RenameService.class;
 	}
 	
 	// contributed by org.eclipse.xtext.xtext.generator.ui.refactoring.RefactorElementNameFragment2
