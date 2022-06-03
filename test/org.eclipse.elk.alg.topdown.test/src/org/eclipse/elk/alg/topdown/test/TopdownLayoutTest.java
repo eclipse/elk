0fb147e8ca4cffd75d2800c0cfc0b4a43b58edd0
Max Kasperowski <mka@informatik.uni-kiel.de>
2022-06-03 15:53:44 +0200

    begin writing topdown layout test

--- test/org.eclipse.elk.alg.topdown.test/src/org/eclipse/elk/alg/topdown/test/TopdownLayoutTest.java
+++ test/org.eclipse.elk.alg.topdown.test/src/org/eclipse/elk/alg/topdown/test/TopdownLayoutTest.java
@@ -9,9 +9,52 @@
  *******************************************************************************/
 package org.eclipse.elk.alg.topdown.test;
 
+import org.eclipse.elk.alg.test.PlainJavaInitialization;
+import org.eclipse.elk.core.math.ElkPadding;
+import org.eclipse.elk.core.options.CoreOptions;
+import org.eclipse.elk.core.options.TopdownNodeTypes;
+import org.eclipse.elk.graph.ElkNode;
+import org.eclipse.elk.graph.util.ElkGraphUtil;
+import org.junit.Test;
+
 /**
  * Tests for checking that topdown layout works correctly in different configurations.
  */
 public class TopdownLayoutTest {
+    
+    // CHECKSTYLEOFF MagicNumber
+    
+    /**
+     * Tests a topdown layout with two hierarchical levels. All nodes are hierarchical nodes.
+     */
+    @Test
+    public void testTwoLevelLayout() {
+        PlainJavaInitialization.initializePlainJavaLayout();
+        ElkNode graph = ElkGraphUtil.createGraph();
+        graph.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        graph.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.ROOT_NODE);
+        
+        ElkNode toplevel = ElkGraphUtil.createNode(graph);
+        toplevel.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        toplevel.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.HIERARCHICAL_NODE);
+        toplevel.setProperty(CoreOptions.NODE_SIZE_FIXED_GRAPH_SIZE, true);
+        toplevel.setProperty(CoreOptions.ALGORITHM, "org.eclipse.elk.layered");
+        toplevel.setProperty(CoreOptions.PADDING, new ElkPadding());
+        toplevel.setProperty(CoreOptions.SPACING_NODE_NODE, 0.0);
+        
+        ElkNode child1 = ElkGraphUtil.createNode(toplevel);
+        child1.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        child1.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.HIERARCHICAL_NODE);
+        child1.setProperty(CoreOptions.NODE_SIZE_FIXED_GRAPH_SIZE, true);
+        
+        ElkNode child2 = ElkGraphUtil.createNode(toplevel);
+        child2.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        child2.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.HIERARCHICAL_NODE);
+        child2.setProperty(CoreOptions.NODE_SIZE_FIXED_GRAPH_SIZE, true);
+        
+        // calle layout with layout engine
+        
+    }
+    
 
 }
