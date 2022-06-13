49ac744e7e20e548e5bdd14bc14edf20c076011c
Max Kasperowski <mka@informatik.uni-kiel.de>
2022-06-13 12:35:32 +0200

    add test for topdown scale cap property

--- test/org.eclipse.elk.alg.topdown.test/src/org/eclipse/elk/alg/topdown/test/TopdownLayoutTest.java
+++ test/org.eclipse.elk.alg.topdown.test/src/org/eclipse/elk/alg/topdown/test/TopdownLayoutTest.java
@@ -138,5 +138,110 @@ public class TopdownLayoutTest {
         assertEquals(30.0 / 70.0, toplevel.getProperty(CoreOptions.TOPDOWN_SCALE_FACTOR), 0.00001);
         
     }
+    
+    /**
+     * Test scale cap property when the result scale is larger than default, but smaller than
+     * the set scale cap.
+     */
+    @Test
+    public void testScaleCap() {
+        PlainJavaInitialization.initializePlainJavaLayout();
+        ElkNode graph = ElkGraphUtil.createGraph();
+        graph.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        graph.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.ROOT_NODE);
+        
+        ElkNode toplevel = ElkGraphUtil.createNode(graph);
+        toplevel.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        toplevel.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.HIERARCHICAL_NODE);
+        toplevel.setProperty(CoreOptions.NODE_SIZE_FIXED_GRAPH_SIZE, true);
+        toplevel.setProperty(CoreOptions.TOPDOWN_SCALE_CAP, 100.0);
+        toplevel.setProperty(CoreOptions.ALGORITHM, "org.eclipse.elk.fixed");
+        toplevel.setProperty(CoreOptions.PADDING, new ElkPadding());
+        toplevel.setProperty(CoreOptions.SPACING_NODE_NODE, 0.0);
+        // set size of node so that children will be scaled down
+        toplevel.setDimensions(300, 300);
+        
+        ElkNode child1 = ElkGraphUtil.createNode(toplevel);
+        child1.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        child1.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.HIERARCHICAL_NODE);
+        child1.setProperty(CoreOptions.NODE_SIZE_FIXED_GRAPH_SIZE, true);
+        child1.setX(0);
+        child1.setY(0);
+        child1.setDimensions(30, 30);
+        
+        ElkNode child2 = ElkGraphUtil.createNode(toplevel);
+        child2.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        child2.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.HIERARCHICAL_NODE);
+        child2.setProperty(CoreOptions.NODE_SIZE_FIXED_GRAPH_SIZE, true);
+        child2.setX(0);
+        child2.setY(40);
+        child2.setDimensions(30, 30);
+        
+        
+        // prepare layout engine
+        LayoutConfigurator config = new LayoutConfigurator();
+        ElkUtil.applyVisitors(graph, config, new LayoutAlgorithmResolver());
+        // call layout with layout engine
+        try {
+            new RecursiveGraphLayoutEngine().layout(graph, new BasicProgressMonitor());
+        } catch (UnsupportedGraphException exception) {
+            fail(exception.toString());
+        }
+               
+        // topdown scale factor of toplevel node
+        assertEquals(300.0 / 70.0, toplevel.getProperty(CoreOptions.TOPDOWN_SCALE_FACTOR), 0.00001);
+    }
 
+
+    /**
+     * Test scale cap property when the scale cap bounds the resulting topdown scale factor.
+     */
+    @Test
+    public void testScaleCapBounded() {
+        PlainJavaInitialization.initializePlainJavaLayout();
+        ElkNode graph = ElkGraphUtil.createGraph();
+        graph.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        graph.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.ROOT_NODE);
+        
+        ElkNode toplevel = ElkGraphUtil.createNode(graph);
+        toplevel.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        toplevel.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.HIERARCHICAL_NODE);
+        toplevel.setProperty(CoreOptions.NODE_SIZE_FIXED_GRAPH_SIZE, true);
+        toplevel.setProperty(CoreOptions.TOPDOWN_SCALE_CAP, 3.0);
+        toplevel.setProperty(CoreOptions.ALGORITHM, "org.eclipse.elk.fixed");
+        toplevel.setProperty(CoreOptions.PADDING, new ElkPadding());
+        toplevel.setProperty(CoreOptions.SPACING_NODE_NODE, 0.0);
+        // set size of node so that children will be scaled down
+        toplevel.setDimensions(300, 300);
+        
+        ElkNode child1 = ElkGraphUtil.createNode(toplevel);
+        child1.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        child1.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.HIERARCHICAL_NODE);
+        child1.setProperty(CoreOptions.NODE_SIZE_FIXED_GRAPH_SIZE, true);
+        child1.setX(0);
+        child1.setY(0);
+        child1.setDimensions(30, 30);
+        
+        ElkNode child2 = ElkGraphUtil.createNode(toplevel);
+        child2.setProperty(CoreOptions.TOPDOWN_LAYOUT, true);
+        child2.setProperty(CoreOptions.TOPDOWN_NODE_TYPE, TopdownNodeTypes.HIERARCHICAL_NODE);
+        child2.setProperty(CoreOptions.NODE_SIZE_FIXED_GRAPH_SIZE, true);
+        child2.setX(0);
+        child2.setY(40);
+        child2.setDimensions(30, 30);
+        
+        
+        // prepare layout engine
+        LayoutConfigurator config = new LayoutConfigurator();
+        ElkUtil.applyVisitors(graph, config, new LayoutAlgorithmResolver());
+        // call layout with layout engine
+        try {
+            new RecursiveGraphLayoutEngine().layout(graph, new BasicProgressMonitor());
+        } catch (UnsupportedGraphException exception) {
+            fail(exception.toString());
+        }
+               
+        // topdown scale factor of toplevel node
+        assertEquals(3.0, toplevel.getProperty(CoreOptions.TOPDOWN_SCALE_FACTOR), 0.00001);
+    }
 }
