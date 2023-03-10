c09f2167a5b34051261a00c22c378400da1762bd
Soeren Domroes <sdo@informatik.uni-kiel.de>
2023-03-10 09:03:24 +0100

    Added breadth first model order layerer + tests.

--- plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/options/LayeringStrategy.java
+++ plugins/org.eclipse.elk.alg.layered/src/org/eclipse/elk/alg/layered/options/LayeringStrategy.java
@@ -11,6 +11,7 @@ package org.eclipse.elk.alg.layered.options;
 
 import org.eclipse.elk.alg.layered.LayeredPhases;
 import org.eclipse.elk.alg.layered.graph.LGraph;
+import org.eclipse.elk.alg.layered.p2layers.BreadthFirstModelOrderLayerer;
 import org.eclipse.elk.alg.layered.p2layers.CoffmanGrahamLayerer;
 import org.eclipse.elk.alg.layered.p2layers.InteractiveLayerer;
 import org.eclipse.elk.alg.layered.p2layers.LongestPathLayerer;
@@ -65,7 +66,13 @@ public enum LayeringStrategy implements ILayoutPhaseFactory<LayeredPhases, LGrap
      * problem with consideration of dummy nodes.
      */
     @ExperimentalPropertyValue
-    MIN_WIDTH;
+    MIN_WIDTH,
+    /**
+     * Assumes an intended breadth first model order of nodes and creates layers accordingly.
+     * No node with a higher model order is in a layer before a node with a lower model order.
+     * Dummy labels are only placed in layers between real nodes.
+     */
+    BF_MODEL_ORDER;
     
     @Override
     public ILayoutPhase<LayeredPhases, LGraph> create() {
@@ -90,6 +97,9 @@ public enum LayeringStrategy implements ILayoutPhaseFactory<LayeredPhases, LGrap
             
         case LONGEST_PATH_SOURCE:
             return new LongestPathSourceLayerer();
+            
+        case BF_MODEL_ORDER:
+            return new BreadthFirstModelOrderLayerer();
 
         default:
             throw new IllegalArgumentException(
---
+++ test/org.eclipse.elk.alg.layered.test/src/org/eclipse/elk/alg/layered/p2layers/BasicBFModelOrderTest.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Kiel University and others.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * SPDX-License-Identifier: EPL-2.0 
+ *******************************************************************************/
+package org.eclipse.elk.alg.layered.p2layers;
+
+import java.util.List;
+
+import org.eclipse.elk.alg.layered.options.CycleBreakingStrategy;
+import org.eclipse.elk.alg.layered.options.LayeredOptions;
+import org.eclipse.elk.alg.layered.options.LayeringStrategy;
+import org.eclipse.elk.alg.test.framework.LayoutTestRunner;
+import org.eclipse.elk.alg.test.framework.annotations.Algorithm;
+import org.eclipse.elk.alg.test.framework.annotations.ConfiguratorProvider;
+import org.eclipse.elk.alg.test.framework.annotations.DefaultConfiguration;
+import org.eclipse.elk.alg.test.framework.annotations.GraphResourceProvider;
+import org.eclipse.elk.alg.test.framework.io.AbstractResourcePath;
+import org.eclipse.elk.alg.test.framework.io.FileExtensionFilter;
+import org.eclipse.elk.alg.test.framework.io.ModelResourcePath;
+import org.eclipse.elk.core.LayoutConfigurator;
+import org.eclipse.elk.graph.ElkNode;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.google.common.collect.Lists;
+
+@RunWith(LayoutTestRunner.class)
+@Algorithm(LayeredOptions.ALGORITHM_ID)
+@DefaultConfiguration()
+public class BasicBFModelOrderTest {
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Sources
+
+    @GraphResourceProvider
+    public List<AbstractResourcePath> testGraphs() {
+        return Lists.newArrayList(
+                new ModelResourcePath("realworld/ptolemy/**/").withFilter(new FileExtensionFilter("elkg")));
+    }
+    
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Configuration
+
+    @ConfiguratorProvider
+    public LayoutConfigurator nodesAndEdgesConfigurator() {
+        LayoutConfigurator config = new LayoutConfigurator();
+        config.configure(ElkNode.class).setProperty(LayeredOptions.CYCLE_BREAKING_STRATEGY,
+                CycleBreakingStrategy.MODEL_ORDER);
+        config.configure(ElkNode.class).setProperty(LayeredOptions.LAYERING_STRATEGY,
+                LayeringStrategy.BF_MODEL_ORDER);
+        return config;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Tests
+    
+    // Just check for errors that might occur
+    
+    @Test
+    public void test(final Object graph) {
+        assert(true);
+    }
+}
